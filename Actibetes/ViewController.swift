b654930a1e43f199dc8258bfc68a9c1eea8d5520
Lanre Durosinmi-Etti <lanre@lanres-air.lan>
2016-02-21 19:01:23 +0000

    Initial Commit

---
+++ Actibetes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Actibetes
+//
+//  Created by Lanre Durosinmi-Etti on 21/02/2016.
+//  Copyright © 2016 mobilehealthcareinc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
